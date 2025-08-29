c8a2f36f0070bbfe1e86efd8d9fd6797feef5e2b
Sebastian D. Eastham <s.eastham@imperial.ac.uk>
2025-08-28 18:24:22 -0600

    Hollow glass sphere

--- Program.cs
+++ Program.cs
@@ -19,14 +19,16 @@ internal class Program
         Material materialGround = new Lambertian(new Vector3d(0.8, 0.8, 0.0));
         Material materialCenter = new Lambertian(new Vector3d(0.1, 0.2, 0.5));
         Material materialLeft = new Dielectric(1.5);
+        Material materialBubble = new Dielectric(1.0 / 1.5);
         Material materialRight = new Metal(new Vector3d(0.8, 0.6, 0.2), 1.0);
 
         // "Ground"
         world.Add(new Sphere(new Vector3d(0.0, -100.5, -1.0), 100.0, materialGround));
         // Sphere directly in front of the camera
-        world.Add(new Sphere(new Vector3d(0.0, 0.0, -1.2), 0.5, materialCenter));
+        world.Add(new Sphere(new Vector3d (0.0, 0.0, -1.2), 0.5, materialCenter));
         world.Add(new Sphere(new Vector3d(-1.0, 0.0, -1.0), 0.5, materialLeft));
         world.Add(new Sphere(new Vector3d(+1.0, 0.0, -1.0), 0.5, materialRight));
+        world.Add(new Sphere(new Vector3d(-1.0, 0.0, -1.0), 0.4, materialBubble));
 
         Camera cam = new()
         {
